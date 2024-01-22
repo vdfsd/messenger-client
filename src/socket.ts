58cc26303259549f0feee294b55e75966fbcd9ed
vdfsd <94445673+vdfsd@users.noreply.github.com>
2024-01-22 13:21:30 +0200

    Update socket.ts

--- src/socket.ts
+++ src/socket.ts
@@ -1,6 +1,7 @@
 import { io } from "socket.io-client"
 
-const API_URL = "http://localhost:4000/"
+// const API_URL = "http://localhost:4000/"
+const API_URL = "https://messenger-server-black.vercel.app/"
 export const socket = io(API_URL, {
   autoConnect: false,
 })
